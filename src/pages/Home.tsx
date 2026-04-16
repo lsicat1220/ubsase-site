1a689330169461b21293c11919ab0875317ef320
lorenzo <xyzhybrid@gmail.com>
2026-04-16 12:33:05 -0400

    content: added first iteration of faq to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,14 @@ export default function Home() {
 				{/* <div>Our Activities</div> */}
 				{/* <Carousel cards={HomePageActivities}/> */}
 			</section>
-			
+			<section className="text-black flex flex-col items-center">
+				<div className="p-20 w-300 text-center">
+					<h1 className="text-4xl pb-10">Frequently Asked Questions</h1>
+					<p className="text-2xl">
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+					</p>
+				</div>
+			</section>
 		</>
 	)
 }
