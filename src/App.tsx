2ed4e72fc643d1845ea314e725a20256101f72fe
tobienguyen <tobey.ahn@gmail.com>
2026-07-11 17:58:29 -0400

    messed up last push, this should be everything up to date

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home.tsx";
 //import Merch from "./pages/Merch.tsx";
 import Leadership from "./pages/Leadership.tsx";
 import Events from "./pages/Events.tsx";
+import About from "./pages/About.tsx";
 //import Contact from "./pages/Contact.tsx";
 import Example from "./pages/ExamplePlaceholder.tsx"
 import Footer from "./components/Footer.tsx";
@@ -18,12 +19,13 @@ function App() {
 			<BrowserRouter>
 				<Navbar />
 				{/*You can imagine that the Routes item below gets replaced with the current url's page*/}
-				<main>
+				<main className="pt-[60px]">
 					<Routes>
 					{/*The path element of the Route tag tells the browser what the link to the page should look like*/}
 						<Route path="/" element={<Home />}/>
 						<Route path="/leadership" element={<Leadership />}/>
 						<Route path="/events" element={<Events />}/>
+						<Route path="/about" element={<About />}/>
 						<Route path="/example" element={<Example />}/>
 					</Routes>
 				</main>
