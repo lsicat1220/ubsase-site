7d5ca7a5826e5905f98d2d32dd8e5b4028583c0b
lorenzo <xyzhybrid@gmail.com>
2026-08-07 22:21:48 -0400

    ui: made a react component for pillar cards and filled out the descriptions

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,20 @@
+interface AboutCardProps {
+	title: string
+	desc: string
+	color: string
+}
+
+function AboutCard({title, desc, color}: AboutCardProps) {
+	return (
+		<>
+				<div className={"w-full flex flex-col p-6 space-y-2 items-center " + color}>
+					<h1 className="text-3xl font-bold text-white italic text-shadow-solid">{title}</h1>
+					<p className="text-lg text-white">{desc}</p>
+				</div>
+		</>
+	)
+}
+
 export default function About() {
 	return (
 		<>
@@ -11,18 +28,21 @@ export default function About() {
 				style={{ backgroundImage: "url('/background/SASE About Us 2.png')" }}
 			>
 				<div className="flex flex-col w-full px-20 mt-32">
-					<div className="w-full bg-blue-500 flex flex-col p-6 space-y-2">
-						<h1 className="text-3xl font-bold text-white">Professional development</h1>
-						<p className="text-lg text-white">yada yada yada</p>
-					</div>
-					<div className="w-full bg-blue-700 flex flex-col p-6 space-y-2 items-end">
-						<h1 className="text-3xl font-bold text-white">Cultural awareness</h1>
-						<p className="text-lg text-white">yada yada yada</p>
-					</div>
-					<div className="w-full bg-green-600 flex flex-col p-6 space-y-2">
-						<h1 className="text-3xl font-bold text-white">Community service</h1>
-						<p className="text-lg text-white">yada yada yada</p>
-					</div>
+					<AboutCard
+						title="PROFESSIONAL DEVELOPMENT"
+						desc="Prepare members for success in the global STEM workforce through professional development and networking"
+						color="bg-blue-700"
+					/>
+					<AboutCard
+						title="CULTURAL AWARENESS"
+						desc="Celebrate and promote diversity across college campuses and modern workplaces"
+						color="bg-green-500"
+					/>
+					<AboutCard
+						title="COMMUNITY SERVICES"
+						desc="Provide a supportive network and encourage local community service"
+						color="bg-blue-600"
+					/>
 				</div>
 			</section>
 		</>
