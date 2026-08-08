bcaea00ee38cc0d79186e187fef3d6705f167020
lorenzo <xyzhybrid@gmail.com>
2026-08-07 22:47:23 -0400

    feat: added a real about us section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ interface AboutCardProps {
 function AboutCard({title, desc, color}: AboutCardProps) {
 	return (
 		<>
-				<div className={"w-full flex flex-col p-6 space-y-2 items-center " + color}>
+				<div className={"w-screen flex flex-col p-6 space-y-2 items-center " + color}>
 					<h1 className="text-3xl font-bold text-white italic text-shadow-solid">{title}</h1>
 					<p className="text-lg text-white">{desc}</p>
 				</div>
@@ -18,16 +18,20 @@ function AboutCard({title, desc, color}: AboutCardProps) {
 export default function About() {
 	return (
 		<>
+			<section className="p-20">
+				<div className="flex flex-row justify-evenly">
+					<div className="flex flex-col space-y-5 my-5">
+						<h1 className="font-bold italic text-black text-5xl">ABOUT US</h1>
+						<p>I'm being forced to write this I don't even go to SASE please help me</p>
+					</div>
+					<div className="bg-red-500 w-100 h-100"></div>
+				</div>
+			</section>
 			<section
-				className="-mt-6 w-screen h-screen bg-cover bg-top bg-no-repeat"
-				style={{ backgroundImage: "url('/background/SASE About Us 1.png')" }}
-			/>
-
-			<section
-				className="w-screen h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center"
-				style={{ backgroundImage: "url('/background/SASE About Us 2.png')" }}
+				className="flex flex-col justify-center items-center border-t-3"
 			>
-				<div className="flex flex-col w-full px-20 mt-32">
+				<h1 className="font-bold italic text-black text-5xl my-10">OUR THREE PILLARS</h1>
+				<div className="flex flex-col w-full">
 					<AboutCard
 						title="PROFESSIONAL DEVELOPMENT"
 						desc="Prepare members for success in the global STEM workforce through professional development and networking"
