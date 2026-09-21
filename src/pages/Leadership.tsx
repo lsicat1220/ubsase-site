68ac57f09faa8e715c8ce3ab4ce1ec032453d246
lorenzo <xyzhybrid@gmail.com>
2026-09-21 03:40:56 -0400

    some pictures are smaller now so they don't lag on the page

--- src/pages/Leadership.tsx
+++ src/pages/Leadership.tsx
@@ -139,7 +139,7 @@ function PopupCard({ member, isSmall, isOpen, dir = 'above' }: { member: Member;
   );
 }
 
-import phone from "../../public/background/leadershipphone.png"
+import phone from "../../public/background/leadershipphonesmall.png"
 import logo from "../../public/background/SASE Logo.png"
 export default function LeadershipSection() {
   const [view, setView] = useState<BoardView>('default');
